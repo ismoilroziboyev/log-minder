7c28676a2829d158d2ee0a579040dc10d52d4f11
Ismoil Ro'ziboyev <ismoil@mf.uz>
2024-03-10 11:01:42 +0500

    changed

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,8 +3,8 @@ package config
 type Config struct {
 	MongoURI      string `env:"MONGO_URI"`
 	MongoDB       string `env:"MONGO_DB"`
-	HttpHost      string `env:"HTTP_HOST"`
-	HttpPort      int    `env:"HTTP_PORT"`
+	HttpHost      string `env:"HTTP_HOST,default=0.0.0.0"`
+	HttpPort      int    `env:"HTTP_PORT,default=35468"`
 	AdminUser     string `env:"ADMIN_USER"`
 	AdminPassword string `env:"ADMIN_PASSWORD"`
 }
